727b122fc0ff316d26456131f553603cf3355b83
Lanoriya <Mr_Buzik@mail.ru>
2024-03-09 18:13:26 +0500

    Фиксы

--- src/js/Characters.js
+++ src/js/Characters.js
@@ -1,45 +1,23 @@
 export default class Character {
   constructor(name, type) {
+    const types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+
     if (typeof name === "string" && name.length >= 2 && name.length <= 10) {
       this.name = name;
     } else {
       throw new Error("Ошибка длинны имени");
     }
-    if (
-      type === "Bowman" ||
-      type === "Swordsman" ||
-      type === "Magician" ||
-      type === "Daemon" ||
-      type === "Undead" ||
-      type === "Zombie"
-    ) {
+    if (types.includes(type)) {
       this.type = type;
     } else {
       throw new Error("Нет такого типа");
     }
     this.health = 100;
     this.level = 1;
-    switch (type) {
-      case "Bowman":
-      case "Undead":
-        this.attack = 25;
-        this.defence = 25;
-        break;
-      case "Swordsman":
-      case "Zombie":
-        this.attack = 40;
-        this.defence = 10;
-        break;
-      case "Magician":
-      case "Daemon":
-        this.attack = 10;
-        this.defence = 40;
-        break;
-      default:
-        break;
-    }
+    this.attack = undefined;
+    this.defence = undefined;
   }
-  
+
   levelUp() {
     if (this.health !== 0) {
       this.level++;
@@ -56,41 +34,5 @@ export default class Character {
     if (this.health >= 0) {
       this.health -= points * (1 - this.defence / 100);
     }
-  }
-}
-
-export class Bowman extends Character {
-  constructor(name) {
-    super(name, 'Bowman');
-  }
-}
-
-export class Swordsman extends Character {
-  constructor(name) {
-    super(name, 'Swordsman');
-  }
-}
-
-export class Magician extends Character {
-  constructor(name) {
-    super(name, 'Magician');
-  }
-}
-
-export class Daemon extends Character {
-  constructor(name) {
-    super(name, 'Daemon');
-  }
-}
-
-export class Undead extends Character {
-  constructor(name) {
-    super(name, 'Undead');
-  }
-}
-
-export class Zombie extends Character {
-  constructor(name) {
-    super(name, 'Zombie');
   }
 }
--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,25 +1,31 @@
-import * as Character from '../Characters';
+import { Character } from '../Characters';
+import { Bowman } from '../Bowman';
+import { Swordsman } from '../Swordsman';
+import { Magician } from '../Magician';
+import { Undead } from '../Undead';
+import { Daemon } from '../Daemon';
+import { Zombie } from '../Zombie';
 
 describe('Check type, Check lvlup, Check name', () => {
   const characterTypes = [
-    { Class: Character.Bowman, type: "Bowman", attack: 25, level: 1 },
-    { Class: Character.Swordsman, type: "Swordsman", attack: 40, level: 1 },
-    { Class: Character.Magician, type: "Magician", attack: 10, level: 1 },
-    { Class: Character.Daemon, type: "Daemon", attack: 10, level: 1 },
-    { Class: Character.Undead, type: "Undead", attack: 25, level: 1 },
-    { Class: Character.Zombie, type: "Zombie", attack: 40, level: 1 },
+    { Class: Bowman, type: "Bowman", attack: 25, level: 1 },
+    { Class: Swordsman, type: "Swordsman", attack: 40, level: 1 },
+    { Class: Magician, type: "Magician", attack: 10, level: 1 },
+    { Class: Daemon, type: "Daemon", attack: 10, level: 1 },
+    { Class: Undead, type: "Undead", attack: 25, level: 1 },
+    { Class: Zombie, type: "Zombie", attack: 40, level: 1 },
   ];
 
   test('Check type', () => {
     characterTypes.forEach(({ Class, type }) => {
-      const personal = new Class("Lanaya")
+      const personal = new Class("Lanaya", type)
       expect(personal.type).toEqual(type)
     })
   })
 
   test('Check lvlup', () => {
-    characterTypes.forEach(({ Class, attack, level }) => {
-      const personal = new Class("Lanaya");
+    characterTypes.forEach(({ Class, type, attack, level }) => {
+      const personal = new Class("Lanaya", type);
       personal.levelUp();
       expect(personal.level).toEqual(level + 1);
       expect(personal.attack).toEqual(attack + attack * 0.2);
@@ -27,8 +33,8 @@ describe('Check type, Check lvlup, Check name', () => {
   })
 
   test('Check name', () => {
-    expect(() => new Character.Bowman("Lanaya")).not.toThrow();
-    expect(() => new Character.Swordsman("L")).toThrow();
-    expect(() => new Character.Magician("LanayaLanaya")).toThrow();
+    expect(() => new Daemon("Lanaya")).not.toThrow();
+    expect(() => new Daemon("L")).toThrow();
+    expect(() => new Daemon("LanayaLanaya")).toThrow();
   })
 })
